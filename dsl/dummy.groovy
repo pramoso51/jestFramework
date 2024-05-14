139a66bd2762651e4d6fda84f7d0e2fa0c955d20
pramoso51 <pramos@integraltecgt.com>
2024-05-13 23:20:27 -0600

    DSL

--- dsl/dummy.groovy
+++ dsl/dummy.groovy
@@ -1,19 +1,24 @@
 folder('mi-carpeta') {
     displayName('Mi Carpeta')
     description('Descripción de Mi Carpeta')
+}
 
-    // pipelineJob('mi-script') {
-    //     displayName('Mi Script')
-    //     description('Script dentro de la carpeta')
+folder('mi-sub-carpeta') {
+    displayName('Mi Sub Carpeta')
+    description('Descripción de Mi Sub Carpeta')
+}
 
-        definition {
-            cps {
-                // Define el script dentro de la definición del trabajo
-                script('''
-                    echo "Hola Mundo"
-                    // Agrega aquí tus comandos o script
-                ''')
-            }
+pipelineJob('mi-script') {
+    displayName('Mi Script')
+    description('Script dentro de la carpeta')
+
+    definition {
+        cps {
+            // Define el script dentro de la definición del trabajo
+            script('''
+                echo "Hola Mundo"
+                // Agrega aquí tus comandos o script
+            ''')
         }
-    // }
+    }
 }
