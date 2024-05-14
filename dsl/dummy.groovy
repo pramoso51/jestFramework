3d84435d99d1d327280bfee6c01fc372fdf9e227
pramoso51 <pramos@integraltecgt.com>
2024-05-13 21:35:56 -0600

    DSL

--- dsl/dummy.groovy
+++ dsl/dummy.groovy
@@ -1,8 +1,39 @@
 folder('dummy-folder') {
-    displayName('Nombre del Folder Dummy')
-    description('Descripcion del Folder Dummy')
+  displayName('Nombre del Folder Dummy')
+  description('Descripcion del Folder Dummy')
 }
 
 folder('dummy-folder/testing-folder') {
-    description('Folder containing all QA jobs for project A')
+  description('Folder containing all QA jobs for project A')
+
+  pipeline {
+    agent any
+    
+    tools {
+        nodejs '22.1.0'
+    }
+
+    stages {
+      stage('Checkout') {
+          steps {
+              git branch: 'main', url: 'https://github.com/pramoso51/jestFramework.git'
+          }
+      }
+      stage('Revisa Version Node') {
+          steps {
+              sh 'npm --version'
+          }
+      }
+      stage('Instalar dependencias') {
+          steps {
+              sh 'npm install'
+          }
+      }
+      stage('Pruebas') {
+          steps {
+              sh 'npm run test'
+          }
+      }
+    }
+  }
 }
