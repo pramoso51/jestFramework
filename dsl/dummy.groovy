affd2f768f92b12732d8cc8b4f0b3ec50ddf5f32
pramoso51 <pramos@integraltecgt.com>
2024-05-13 22:51:00 -0600

    DSL

--- dsl/dummy.groovy
+++ dsl/dummy.groovy
@@ -1,47 +1,42 @@
-folder('dummy-folder') {
-    displayName('Nombre del Folder Dummy')
-    description('Descripcion del Folder Dummy')
+pipelineJob('dummy-folder/Testing Jest') {
+    displayName('Testing Jest Pipeline')
+    description('Pipeline for running Jest tests')
 
-    pipelineJob('Testing Jest') {
-        displayName('Testing Jest Pipeline')
-        description('Pipeline for running Jest tests')
-
-        definition {
-            cps {
-                script('''
-                    pipeline {
-                        agent any
-                        tools {
-                            nodejs '22.1.0'
+    definition {
+        cps {
+            script('''
+                pipeline {
+                    agent any
+                    tools {
+                        nodejs '22.1.0'
+                    }
+                    stages {
+                        stage('Checkout') {
+                            steps {
+                                git branch: 'main', url: 'https://github.com/pramoso51/jestFramework.git'
+                            }
                         }
-                        stages {
-                            stage('Checkout') {
-                                steps {
-                                    git branch: 'main', url: 'https://github.com/pramoso51/jestFramework.git'
-                                }
+                        stage('Revisa Version Node') {
+                            steps {
+                                sh 'npm --version'
                             }
-                            stage('Revisa Version Node') {
-                                steps {
-                                    sh 'npm --version'
-                                }
+                        }
+                        stage('Instalar dependencias') {
+                            steps {
+                                sh 'npm install'
                             }
-                            stage('Instalar dependencias') {
-                                steps {
-                                    sh 'npm install'
-                                }
-                            }
-                            stage('Pruebas') {
-                                steps {
-                                    sh 'npm run test'
-                                }
+                        }
+                        stage('Pruebas') {
+                            steps {
+                                sh 'npm run test'
                             }
                         }
                     }
-                ''')
-            }
+                }
+            ''')
         }
     }
     triggers {
-            cron('@after_build') 
-        }
+        cron('* * * * *') // Ejecutar cada 5 minutos, por ejemplo
+    }
 }
