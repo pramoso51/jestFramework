8a32a3fc270c581eb2c30b4b1ac8e3f9ef16f2bb
pramoso51 <pramos@integraltecgt.com>
2024-05-13 21:51:28 -0600

    DSL

--- dsl/dummy.groovy
+++ dsl/dummy.groovy
@@ -1,10 +1,6 @@
 folder('dummy-folder') {
     displayName('Nombre del Folder Dummy')
     description('Descripcion del Folder Dummy')
-}
-
-folder('dummy-folder/testing-folder') {
-    description('Folder containing all QA jobs for project A')
 
     pipelineJob('Testing Jest') {
         displayName('Testing Jest Pipeline')
@@ -12,37 +8,36 @@ folder('dummy-folder/testing-folder') {
 
         definition {
             cps {
-                script {
-                    // Define la versión de Node.js para este pipeline
-                    tools {
-                        nodejs '22.1.0'
+                script("""
+                    pipeline {
+                        agent any
+                        tools {
+                            nodejs '22.1.0'
+                        }
+                        stages {
+                            stage('Checkout') {
+                                steps {
+                                    git branch: 'main', url: 'https://github.com/pramoso51/jestFramework.git'
+                                }
+                            }
+                            stage('Revisa Version Node') {
+                                steps {
+                                    sh 'npm --version'
+                                }
+                            }
+                            stage('Instalar dependencias') {
+                                steps {
+                                    sh 'npm install'
+                                }
+                            }
+                            stage('Pruebas') {
+                                steps {
+                                    sh 'npm run test'
+                                }
+                            }
+                        }
                     }
-
-                    // Define los pasos del pipeline dentro de node
-                    node {
-                        // Definición de tu pipeline aquí
-                        stage('Checkout') {
-                            steps {
-                                git branch: 'main', url: 'https://github.com/pramoso51/jestFramework.git'
-                            }
-                        }
-                        stage('Revisa Version Node') {
-                            steps {
-                                sh 'npm --version'
-                            }
-                        }
-                        stage('Instalar dependencias') {
-                            steps {
-                                sh 'npm install'
-                            }
-                        }
-                        stage('Pruebas') {
-                            steps {
-                                sh 'npm run test'
-                            }
-                        }
-                    }
-                }
+                """)
             }
         }
     }
