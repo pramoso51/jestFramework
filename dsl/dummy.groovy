88911714e85f59564c80d9b4f7208ccbbb6e781c
pramoso51 <pramos@integraltecgt.com>
2024-05-14 00:35:28 -0600

    DSL

--- dsl/dummy.groovy
+++ dsl/dummy.groovy
@@ -22,7 +22,7 @@ pipelineJob('mi-carpeta/mi-sub-carpeta/mi-script') {
     }
 }
 
-job("mi-carpeta/mi-sub-carpeta/mi-script") {
+job("Seed 0") {
     steps {
         shell ('''
             echo mi-carpeta/mi-sub-carpeta/mi-script
