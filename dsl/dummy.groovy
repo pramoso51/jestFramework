76f4d92cc41bfcb826c8380213ced81cc3467dae
pramoso51 <pramos@integraltecgt.com>
2024-05-13 23:31:51 -0600

    DSL

--- dsl/dummy.groovy
+++ dsl/dummy.groovy
@@ -1,26 +1,28 @@
+// Define la estructura de carpetas y el trabajo de Pipeline
 folder('mi-carpeta') {
     displayName('Mi Carpeta')
     description('Descripción de Mi Carpeta')
-}
+    
+    folder('mi-sub-carpeta') {
+        displayName('Mi Sub Carpeta')
+        description('Descripción de Mi Sub Carpeta')
 
-folder('mi-carpeta/mi-sub-carpeta') {
-    displayName('Mi Sub Carpeta')
-    description('Descripción de Mi Sub Carpeta')
-}
+        pipelineJob('mi-script') {
+            displayName('Mi Script')
+            description('Script dentro de la carpeta')
 
-pipelineJob('mi-carpeta/mi-sub-carpeta/mi-script') {
-    displayName('Mi Script')
-    description('Script dentro de la carpeta')
-
-    definition {
-        cps {
-            // Define el script dentro de la definición del trabajo
-            script('''
-                echo "Hola Mundo"
-                // Agrega aquí tus comandos o script
-            ''')
+            definition {
+                cps {
+                    // Define el script dentro de la definición del trabajo
+                    script('''
+                        echo "Hola Mundo"
+                        // Agrega aquí tus comandos o script
+                    ''')
+                }
+            }
         }
     }
 }
 
+// Ejecutar el trabajo de Pipeline
 build job: 'mi-carpeta/mi-sub-carpeta/mi-script', wait: true
