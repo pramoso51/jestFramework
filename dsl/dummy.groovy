a15e0c5376169b5353075873f5f6688eb28c1696
pramoso51 <pramos@integraltecgt.com>
2024-05-13 23:50:52 -0600

    DSL

--- dsl/dummy.groovy
+++ dsl/dummy.groovy
@@ -1,29 +1,13 @@
-import groovy.json.JsonBuilder
-import groovy.util.XmlParser
-import jenkins.model.Jenkins
-
-def jenkins = Jenkins.getInstance()
-
-def script = '''
-folder('mi-folder') {
-    displayName('Mi Folder')
-    description('Folder para organizar jobs')
-
-    pipelineJob('hola-mundo') {
-        displayName('Hola Mundo')
-        description('Pipeline que imprime Hola Mundo')
-
-        definition {
-            cps {
-                script('''
-                    // Aquí va el script del pipeline
-                    println "Hola Mundo"
-                ''')
+pipeline {
+    agent any
+    tools {
+        nodejs '22.1.0'
+    }
+    stages {
+        stage('Checkout') {
+            steps {
+                echo "Hola Mundo"
             }
         }
     }
 }
-'''
-
-def jobXml = new XmlParser().parseText(script)
-jenkins.createProjectFromXML("mi-folder", new JsonBuilder(jobXml).toString())
